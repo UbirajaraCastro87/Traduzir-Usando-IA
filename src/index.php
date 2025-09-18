cd5419d5194ed110b0b9f37bd76f2a34663bdd1a
UbirajaraCastro87 <167134167+UbirajaraCastro87@users.noreply.github.com>
2025-09-18 13:57:47 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Fluent_IA</title>
+    <title>IA</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="shortcut icon" href="./src/views/asset/favicon.ico" type="image/x-icon" />
   </head>
